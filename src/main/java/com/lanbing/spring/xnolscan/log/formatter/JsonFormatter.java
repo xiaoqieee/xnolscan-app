a69054391a31db2ce377fbcca057f80eaa2e67ce
xiaoqieee <fengqi3389@sina.com>
2019-02-12 22:14:14 +0800

    kafka 日志

--- src/main/java/com/lanbing/spring/xnolscan/log/formatter/JsonFormatter.java
+++ src/main/java/com/lanbing/spring/xnolscan/log/formatter/JsonFormatter.java
@@ -11,61 +11,61 @@ import java.util.*;
 
 public class JsonFormatter implements Formatter {
 
-  private boolean expectJsonMessage = false;
-  private boolean includeMethodAndLineNumber = false;
-  private Map extraPropertiesMap = null;
+    private boolean expectJsonMessage = false;
+    private boolean includeMethodAndLineNumber = false;
+    private Map extraPropertiesMap = null;
 
-  public String format(ILoggingEvent event) {
-    JSONObject jsonObject = new JSONObject();
-    jsonObject.put("level", event.getLevel().levelStr);
-    jsonObject.put("class", event.getLoggerName());
-    jsonObject.put("timestamp",DateUtils.getTimeString(new Date(event.getTimeStamp())));
-    jsonObject.put("message", event.getFormattedMessage());
+    public String format(ILoggingEvent event) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("level", event.getLevel().levelStr);
+        jsonObject.put("class", event.getLoggerName());
+        jsonObject.put("timestamp", DateUtils.getTimeString(new Date(event.getTimeStamp())));
+        jsonObject.put("message", event.getFormattedMessage());
 
-    if (includeMethodAndLineNumber) {
-      StackTraceElement[] callerDataArray = event.getCallerData();
-      if (callerDataArray != null && callerDataArray.length > 0) {
-        StackTraceElement stackTraceElement = callerDataArray[0];
-        jsonObject.put("method", stackTraceElement.getMethodName());
-        jsonObject.put("line", stackTraceElement.getLineNumber() + "");
-      }
+        if (includeMethodAndLineNumber) {
+            StackTraceElement[] callerDataArray = event.getCallerData();
+            if (callerDataArray != null && callerDataArray.length > 0) {
+                StackTraceElement stackTraceElement = callerDataArray[0];
+                jsonObject.put("method", stackTraceElement.getMethodName());
+                jsonObject.put("line", stackTraceElement.getLineNumber() + "");
+            }
+        }
+        if (this.extraPropertiesMap != null) {
+            jsonObject.putAll(extraPropertiesMap);
+        }
+        return jsonObject.toJSONString();
     }
-    if (this.extraPropertiesMap != null) {
-      jsonObject.putAll(extraPropertiesMap);
+
+    public boolean getExpectJsonMessage() {
+        return expectJsonMessage;
     }
-    return jsonObject.toJSONString();
-  }
 
-  public boolean getExpectJsonMessage() {
-    return expectJsonMessage;
-  }
-
-  public void setExpectJsonMessage(boolean expectJsonMessage) {
-    this.expectJsonMessage = expectJsonMessage;
-  }
-
-  public boolean getIncludeMethodAndLineNumber() {
-    return includeMethodAndLineNumber;
-  }
-
-  public void setIncludeMethodAndLineNumber(boolean includeMethodAndLineNumber) {
-    this.includeMethodAndLineNumber = includeMethodAndLineNumber;
-  }
-
-  public void setExtraProperties(String thatExtraProperties) {
-    final Properties properties = new Properties();
-    try {
-      properties.load(new StringReader(thatExtraProperties));
-      Enumeration enumeration = properties.propertyNames();
-      extraPropertiesMap = new HashMap();
-      while(enumeration.hasMoreElements()){
-        String name = (String)enumeration.nextElement();
-        String value = properties.getProperty(name);
-        extraPropertiesMap.put(name,value);
-      }
-    } catch (IOException e) {
-      System.out.println("There was a problem reading the extra properties configuration: "+e.getMessage());
-      e.printStackTrace();
+    public void setExpectJsonMessage(boolean expectJsonMessage) {
+        this.expectJsonMessage = expectJsonMessage;
     }
-  }
+
+    public boolean getIncludeMethodAndLineNumber() {
+        return includeMethodAndLineNumber;
+    }
+
+    public void setIncludeMethodAndLineNumber(boolean includeMethodAndLineNumber) {
+        this.includeMethodAndLineNumber = includeMethodAndLineNumber;
+    }
+
+    public void setExtraProperties(String thatExtraProperties) {
+        final Properties properties = new Properties();
+        try {
+            properties.load(new StringReader(thatExtraProperties));
+            Enumeration enumeration = properties.propertyNames();
+            extraPropertiesMap = new HashMap();
+            while (enumeration.hasMoreElements()) {
+                String name = (String) enumeration.nextElement();
+                String value = properties.getProperty(name);
+                extraPropertiesMap.put(name, value);
+            }
+        } catch (IOException e) {
+            System.out.println("There was a problem reading the extra properties configuration: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/lanbing/spring/xnolscan/log/formatter/MessageFormatter.java
+++ src/main/java/com/lanbing/spring/xnolscan/log/formatter/MessageFormatter.java
@@ -3,11 +3,9 @@ package com.lanbing.spring.xnolscan.log.formatter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
 /**
- * 
  * Formatter implementation that simply returns the logback message.
- * 
+ *
  * @author tgoetz
- * 
  */
 public class MessageFormatter implements Formatter {
 
