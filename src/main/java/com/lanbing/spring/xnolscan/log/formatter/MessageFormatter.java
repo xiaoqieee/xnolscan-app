c0e2eb8710929f3343a779b66b9eb2da9be5cd9e
xiaoqieee <fengqi3389@sina.com>
2019-02-12 22:14:54 +0800

    kafka 日志

--- src/main/java/com/lanbing/spring/xnolscan/log/formatter/MessageFormatter.java
+++ src/main/java/com/lanbing/spring/xnolscan/log/formatter/MessageFormatter.java
@@ -5,7 +5,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 /**
  * Formatter implementation that simply returns the logback message.
  *
- * @author tgoetz
+ * @author fengqi
  */
 public class MessageFormatter implements Formatter {
 
