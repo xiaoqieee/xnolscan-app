1306ff36124e75c614fa9c4e31db1df1a10eab60
xiaoqieee <fengqi3389@sina.com>
2019-02-18 14:51:47 +0800

    大数字

--- src/main/java/com/lanbing/spring/xnolscan/helper/ProductCanBuyHelper.java
+++ src/main/java/com/lanbing/spring/xnolscan/helper/ProductCanBuyHelper.java
@@ -1,52 +1,13 @@
 package com.lanbing.spring.xnolscan.helper;
 
-import com.lanbing.spring.xnolscan.constant.Constants;
 import com.lanbing.spring.xnolscan.model.Product;
 import com.lanbing.spring.xnolscan.util.DataToDiscUtils;
 import com.lanbing.spring.xnolscan.util.DecimalUtil;
-import org.apache.kafka.common.protocol.types.Field;
 
 import java.math.BigDecimal;
-import java.text.Bidi;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductCanBuyHelper {
-
-    public static boolean canBuy_(Product t) {
-        if (DecimalUtil.le(t.getLeftAmount(), BigDecimal.ZERO)) {
-            return false;
-        }
-
-        if (DecimalUtil.gt(t.getLeftAmount(), BigDecimal.valueOf(5000))) {
-            if (t.getProductTerm() < 90 && t.getTargetRatio() > 0.5) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        if (t.getProductTerm() < 60 && t.getTargetRatio() > 0.25) {
-            return true;
-        }
-
-        if (t.getProductTerm() < 90 && t.getTargetRatio() > 0.27) {
-            return true;
-        }
-
-        if (t.getProductTerm() < 120 && t.getTargetRatio() > 0.33) {
-            return true;
-        }
-
-        if (t.getProductTerm() < 180 && t.getTargetRatio() > 0.38) {
-            return true;
-        }
-
-        if (t.getProductTerm() < 240 && t.getTargetRatio() > 0.42) {
-            return true;
-        }
-        return false;
-    }
 
     public static boolean canBuy(Product t) {
         if (DecimalUtil.le(t.getLeftAmount(), BigDecimal.ZERO)) {
