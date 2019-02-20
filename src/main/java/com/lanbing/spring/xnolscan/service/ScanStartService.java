53f2522c8894df73341181caf41b042988d67124
xiaoqieee <fengqi3389@sina.com>
2019-02-20 13:44:52 +0800

    大数字

--- src/main/java/com/lanbing/spring/xnolscan/service/ScanStartService.java
+++ src/main/java/com/lanbing/spring/xnolscan/service/ScanStartService.java
@@ -1,10 +1,7 @@
 package com.lanbing.spring.xnolscan.service;
 
-import com.lanbing.spring.xnolscan.constant.Constants;
-import com.lanbing.spring.xnolscan.helper.HttpHeaderHelper;
-import com.lanbing.spring.xnolscan.helper.ProductMaxIdHelper;
-import com.lanbing.spring.xnolscan.helper.RequestTokenHelper;
-import com.lanbing.spring.xnolscan.helper.StatusHelper;
+import com.lanbing.spring.xnolscan.constant.ConfigKey;
+import com.lanbing.spring.xnolscan.helper.*;
 import com.lanbing.spring.xnolscan.thread.DetailScanTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,7 +19,7 @@ public class ScanStartService extends BaseService {
     @Autowired
     private XnolDetailScanService xnolDetailScanService;
 
-    public String start(Integer baseProductId){
+    public String start(Integer baseProductId) {
 
         if (StatusHelper.canStart()) {
 
@@ -51,8 +48,8 @@ public class ScanStartService extends BaseService {
     private void startDetail() {
         // 根据累加ID搜索
         final int currentMaxId = ProductMaxIdHelper.currentMaxProductId.get() - 1;
-        int threadCountPerProductId = Constants.SCAN_DETAIL_THREAD_COUNT_PER_ID;
-        int step = Constants.SCAN_DETAIL_STOP;
+        int threadCountPerProductId = Integer.valueOf(BizConfigHelper.get(ConfigKey.DETAIL_THREAD_COUNT, "2"));
+        int step = Integer.valueOf(BizConfigHelper.get(ConfigKey.DETAIL_SCAN_STEP, "20"));
 
         for (int interval = 0; interval < step; interval++) {
             for (int i = 0; i < threadCountPerProductId; i++) {
