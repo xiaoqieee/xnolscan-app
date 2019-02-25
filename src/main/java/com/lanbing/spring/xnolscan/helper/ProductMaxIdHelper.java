b5267877ebbac2d76897d286dd241ea667616f86
xiaoqieee <fengqi3389@sina.com>
2019-02-25 21:56:52 +0800

    限流控制策略调优

--- src/main/java/com/lanbing/spring/xnolscan/helper/ProductMaxIdHelper.java
+++ src/main/java/com/lanbing/spring/xnolscan/helper/ProductMaxIdHelper.java
@@ -9,7 +9,7 @@ public class ProductMaxIdHelper {
 
     private static Object lock = new Object();
 
-    public static AtomicInteger currentMaxProductId = new AtomicInteger(49225897);
+    public static AtomicInteger currentMaxProductId = new AtomicInteger(49229464);
 
     public static void init(int baseProductId) {
         int maxProductId = intelligenceGetMaxId(baseProductId);
