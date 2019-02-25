b387f08bd6c071932c35b04a90b057fec3e805c0
xiaoqieee <fengqi3389@sina.com>
2019-02-26 07:43:50 +0800

    大数字

--- src/main/java/com/lanbing/spring/xnolscan/model/ProductIdBO.java
+++ src/main/java/com/lanbing/spring/xnolscan/model/ProductIdBO.java
@@ -64,7 +64,7 @@ public class ProductIdBO {
         if (hour >= 0 && hour <= 4) {
             return 100;
         }
-        if(custProductId < ProductMaxIdHelper.currentMaxProductId.get() - 2 * step){
+        if(custProductId < ProductMaxIdHelper.currentMaxProductId.get() - step - 1){
             return getId(custProductId,ProductMaxIdHelper.currentMaxProductId.get(),step);
         }
         return step;
